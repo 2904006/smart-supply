8f18d59604682a562796f75fd6d7e887332af474
Khadija Ait Bella <khadija.aitbella.estn24@ump.ac.ma>
2026-03-13 19:24:40 +0000

    Add authentication service functions for login, register, and logout

---
+++ src/utils/authService.js
@@ -0,0 +1,13 @@
+// authService.js
+
+export const login = async (email, password) => {
+    // Logic for logging in the user
+};
+
+export const register = async (userData) => {
+    // Logic for registering a new user
+};
+
+export const logout = () => {
+    // Logic for logging out the user
+};
