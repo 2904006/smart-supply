ebc00375db4dc7e55ef68acc7637f1c0b713c3a8
Khadija Ait Bella <khadija.aitbella.estn24@ump.ac.ma>
2026-03-13 19:24:56 +0000

    Create productService.js with product service functions

---
+++ src/utils/productService.js
@@ -0,0 +1,26 @@
+// productService.js
+
+// Function to get product details by ID
+export const getProductById = (id) => {
+    // Implement fetching logic here
+};
+
+// Function to get all products
+export const getAllProducts = () => {
+    // Implement fetching logic here
+};
+
+// Function to create a new product
+export const createProduct = (product) => {
+    // Implement creation logic here
+};
+
+// Function to update an existing product
+export const updateProduct = (id, updatedProduct) => {
+    // Implement update logic here
+};
+
+// Function to delete a product
+export const deleteProduct = (id) => {
+    // Implement deletion logic here
+};
